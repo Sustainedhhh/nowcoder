c91f57fb9948156f71fa5f5b0d6ef0da3a28df4e
zgl <tigustavo@foxmail.com>
2019-03-17 11:55:48 +0800

    回文还是错的 要改

---
+++ task/src/ValidPalindrome125.java
@@ -0,0 +1,76 @@
+public class ValidPalindrome125 {
+
+
+
+    /*
+    Example 1:
+
+    Input: "A man, a plan, a canal: Panama"
+    Output: true
+    Example 2:
+
+    Input: "race a car"
+    Output: false*/
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(isPalindrome("race a car"));
+        System.out.println(isPalindrome("0p"));
+
+//        System.out.println(String.valueOf('a').toUpperCase());
+//        System.out.println(String.valueOf('A').toUpperCase());
+//        System.out.println(String.valueOf('A').toUpperCase().equals(String.valueOf('a').toUpperCase()));
+
+
+    }
+
+//    public static boolean isPalindrome(String s) {
+//        if(s == ""){
+//            return true;
+//        }
+//        else{
+//            int low = 0;
+//            int high = s.length()-1;
+//            char lowChar,highChar;
+//            while(low < high){
+//                lowChar = s.charAt(low);
+//                highChar = s.charAt(high);
+//                while(low<high && !Character.isAlphabetic((int)lowChar)){
+//                    low++;
+//                    if(low<high){
+//                        lowChar = s.charAt(low);
+//                    }
+//                }
+//                while(low<high && !Character.isAlphabetic((int)highChar)){
+//                    high--;
+//                    if(low<high){
+//                        highChar = s.charAt(high);
+//                    }
+//                }
+//                if(low<high) {
+//                    if (String.valueOf(lowChar).toUpperCase().equals(String.valueOf(highChar).toUpperCase())) {
+//                        low++;
+//                        high--;
+//                    } else {
+//                        return false;
+//                    }
+//                }
+//            }
+//            return true;
+//        }
+//    }
+    public static boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) return true;
+        int i = 0, j = s.length() - 1;
+        s = s.toLowerCase();
+        while (i < j) {
+            final char ic = s.charAt(i);
+            final char jc = s.charAt(j);
+            if (!Character.isLetterOrDigit(ic)) { i++; continue; }
+            if (!Character.isLetterOrDigit(jc)) { j--; continue; }
+            if (ic != jc) return false;
+            i++; j--;
+        }
+        return true;
+    }
+}
---
+++ task/src/twosum167.java
@@ -0,0 +1,37 @@
+public class twosum167 {
+
+
+
+/*
+    Input: numbers = [2,7,11,15], target = 9
+    Output: [1,2]
+    Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+*/
+
+    public static void main(String[] args) {
+        int[] numbers = {2,7,11,15};
+        int target = 9;
+        myUtil.printArray(twoSum(numbers,target));
+    }
+
+
+
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int low = 0;
+        int high = numbers.length-1;
+        while(low<high){
+            if(low<high && numbers[low]+numbers[high] ==target){
+                int[] res = {low+1,high+1};
+                return res;
+            }
+            if(low < high && numbers[low]+numbers[high] < target){
+                low++;
+            }
+            if(low < high && numbers[low]+numbers[high]>target){
+                high--;
+            }
+        }
+        return null;
+    }
+}
